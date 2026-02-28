79ef3b6cf8af430a48314c7b4dd125eabcd616ba
Alicia Tesch <tescha13@icloud.com>
2026-02-28 22:24:03 +0000

    update for vines, butterflies, and bees

---
+++ script.js
@@ -0,0 +1,10 @@
+// Chemis-Tea Party JS
+// Game logic will be here later
+
+console.log("Garden loaded 🌿");
+
+// Future ideas:
+// - Mouse-follow pollinator
+// - Flower collision detection
+// - Pollination animations
+// - Info popups
